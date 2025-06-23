93f8214d3b133e478894ac6ebf6ac28898abbfe6
Lena <lenatsivunina@gmail.com>
2025-06-23 12:19:21 +1000

    коммит без JSX

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,46 @@
 {/*декларативный*/}
 import logo from './assets/logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-export const App = () => { {/*императивный*/}
-{/*декларативный*/}
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-		<p>{new Date().getFullYear()}</p> {/*декларативный*/}
-      </header>
-    </div>
-  );
+export const App = () => {
+  return createElement(
+	"div",
+	{className : "App"},
+	createElement(
+		"header",
+		{className: "App-header"},
+		createElement(
+			"img",
+			{
+				src: logo,
+				className: "App-logo",
+				alt: "logo"
+			}
+		),
+		createElement(
+			"p",
+			null,
+			"Edit",
+			createElement("code", null, "src/App.js"),
+			" and save to reload."
+		),
+		createElement(
+			"a",
+			{
+				className: "App-link",
+				href: "https://reactjs.org",
+				target: "_blank",
+				rel: "noopener noreferrer"
+			},
+        	"Learn React"
+		),
+		createElement(
+			"p",
+			null,
+			new Date().getFullYear()
+		)
+	)
+)
 }
 
