854ac80f7bc8a8724937d33380527b19337f6477
Lena <lenatsivunina@gmail.com>
2025-06-23 12:11:05 +1000

    коммит с JSX

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+{/*декларативный*/}
+import logo from './assets/logo.svg';
+import './App.css';
+
+export const App = () => { {/*императивный*/}
+{/*декларативный*/}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+		<p>{new Date().getFullYear()}</p> {/*декларативный*/}
+      </header>
+    </div>
+  );
+}
+
